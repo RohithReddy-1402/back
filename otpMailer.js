a0b84663f500e90ce820c7f2a2e7b22da3246607
RohithReddy <r14v18@gmail.com>
2025-06-09 00:42:27 +0530

    cahnges

--- otpMailer.js
+++ otpMailer.js
@@ -338,7 +338,7 @@ const sendOTP = async (toEmail,name, otp) => {
         <div class="email-container">
             <!-- Header -->
             <div class="header">
-                <a href="https://nitkkrpreviouspapers.vercel.app" class="header-logo">
+                <a href="https://nitkkrpreviouspapers.vercel.app" class="header-logo" style="text-decoration: none; color: white;">
                     NIT KKR Previous Papers
                 </a>
                 
