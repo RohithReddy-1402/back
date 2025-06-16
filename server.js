109e8e67a9d68088fc69cc4cb0cdf40678c1b519
RohithReddy <r14v18@gmail.com>
2025-06-17 00:07:34 +0530

    changes

--- server.js
+++ server.js
@@ -194,7 +194,7 @@ app.post('/login', async (req, res) => {
     const token = setUser(user);
     res.cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure:true,
         sameSite: "None",
         maxAge: 30 * 24 * 60 * 60 * 1000,
       })
