d07daf0888f4a619e2549ee8cef8cdec1e1eeab6
RohithReddy-1402 <r14v18@gmail.com>
2025-08-28 12:42:58 +0530

    changes

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const corsOptions = {
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', "PATCH", 'OPTIONS'],
 };
 
 cloudinary.config({
